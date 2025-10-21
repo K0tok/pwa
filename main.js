a28f6b210524e76386cc89246e3da7297a615382
Konstantin <bodrov.2005@inbox.ru>
2025-10-21 20:24:18 +0500

    .

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const height_elem = document.getElementById('height')
+    const weight_elem = document.getElementById('weight')
+    const result_elem = document.getElementById('result')
+    const btn_elem = document.getElementById('btn')
+    
+    btn_elem.addEventListener('click', function(){
+        let h = (+height_elem.value) / 100
+        let w = +weight_elem.value
+
+        let imt = w / (h * h)
+
+        result_elem.value = imt.toFixed(2)
+    })
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+                console.log('SW registered', registration)
+            })
+            .catch(error => {
+                console.log('SW failed', error)
+            })
+    }
+})
